473b050a88a19aa7677c895835b3856113154066
penko <ksenyap5553750@gmail.com>
2022-12-10 03:04:14 +1000

    домашка готова

--- src/main/kotlin/homeWork3/Main.kt
+++ src/main/kotlin/homeWork3/Main.kt
@@ -1,5 +1,38 @@
 package homeWork3
 
 fun main() {
+    var firstName = "Kseniia"
+    var lastName = "Pe"
+    var height: Double = 50000.0
+    var weightFloat = 35
+    var child = "Личинка"
+    var isChild: Boolean = (height < 150 && weightFloat < 40)
+    if (isChild) {
+        println("is she a Child? - $isChild")
+        child = "Личинка"
+    } else {
+        println("is she a Child? - $isChild")
+        child = "Взрослый лох"
+    }
+
+    var info = ("$firstName $lastName $height $weightFloat $child")
+    println(info)
+    height = 10.0
+    isChild = (height < 150 || weightFloat < 40)
+    if (isChild) {
+        println("is she a Child? - $isChild")
+        child = "Личинка"
+    } else {
+        println("is she a Child? - $isChild")
+        child = "Взрослый лох"
+    }
+
+
+    info = ("$firstName $lastName $height $weightFloat $child")
+    println(info)
+
+
+
+
 
 }
