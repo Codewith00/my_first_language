8dd31fc0799c3f5605fc8354fc9c9d4c18378ce6
Anton <master0jide@gmail.com>
2022-12-10 14:18:53 +0600

    Круто быть программистом

--- src/main/kotlin/homeWork4/ForCheck.kt
+++ src/main/kotlin/homeWork4/ForCheck.kt
@@ -2,16 +2,18 @@ package homeWork4
 
 fun main() {
     val message = "F2p)v\"y233{0->c}ttelciFc"
-    val firstHalf = message.substring(0, message.count()/2)
-    val secondHalf = message.substring(message.count()/2, message.count())
+    val firstHalf = message.substring(0, message.count() / 2)
+    val secondHalf = message.substring(message.count() / 2, message.count())
 
     println("${first(firstHalf)} ${second(secondHalf)}".uppercase())
 
 }
-fun shift(mess: String, a: Int) : String{
+
+fun shift(mess: String, a: Int): String {
     return mess.map { it + a }.joinToString("")
 
 }
+
 fun first(message: String): String {
     val currentMess = shift(message, 1)
         .replace("5", "s")
@@ -19,8 +21,9 @@ fun first(message: String): String {
     return shift(currentMess, -3).replace("0", "o")
 
 }
+
 fun second(message: String): String {
- val currentMess = message.reversed()
+    val currentMess = message.reversed()
     return shift(currentMess, -4).replace("_", " ")
 }
 
